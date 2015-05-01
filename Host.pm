9dd6f89ba4dffa631e41ed62e9de6b1b49cc83cb
Tuomo Hartikainen <tth@harski.org>
2015-05-01 22:02:28 +0300

    Initial commit

---
+++ Host.pm
@@ -0,0 +1,131 @@
+package Host;
+
+use strict;
+use warnings;
+use v5.20;
+use Fcntl qw(:flock SEEK_END);
+use POSIX qw(strftime);
+
+sub DEBUG { 0; };
+
+
+sub do_ping {
+	my $self = shift;
+
+	`ping -c 1 $self->{addr}`;
+	$self->{pings} += 1;
+	$self->log_ping();
+
+	if ($? == 0) {
+		$self->{failed_pings} = 0;
+	} else {
+		$self->{failed_pings} += 1;
+	}
+}
+
+
+sub new {
+	my ($class, $name, $addr, $logfile) = @_;
+
+	my $self = {
+			name	=> $name,
+			addr	=> $addr,
+			logfile	=> $logfile,
+
+			allowed_failed_pings	=> 2,
+			failed_pings		=> 0,
+			is_up			=> -1, # unknown
+			log_pings		=> 0,
+			old_status		=> -1, # unknown
+			ping_delay		=> 30,
+			pings			=> 0,
+		   };
+
+	bless($self, $class);
+	return $self;
+}
+
+
+sub log {
+	my ($self, $msg) = @_;
+
+	my $timestr = strftime " %Y%d%m-%H%M%S", localtime;
+
+	open my $lfh, '>>', $self->{logfile};
+	flock($lfh, LOCK_EX);
+	seek($lfh, 0, SEEK_END);
+
+	print $lfh "$timestr: host($self->{name}): $msg\n";
+
+	close $lfh;
+}
+
+
+sub log_statuschange {
+	my $self = shift;
+
+	my $msg = "status changed to ";
+	$msg .= $self->{is_up} ? 'UP' : 'DOWN';
+
+	$self->log($msg);
+}
+
+
+sub log_ping {
+	my $self = shift;
+	return if not $self->{log_pings};
+
+	my $msg = "";
+
+	if ($self->{failed_pings} == 0) {
+		$msg .= "ping succeeded";
+	} else {
+		$msg .= "ping $self->{failed_pings}/$self->{allowed_failed_pings} failed";
+	}
+
+	$self->log($msg);
+}
+
+
+sub ping {
+	my $self = shift;
+
+	if (DEBUG) { $self->{log_pings} = 1; }
+
+	# TODO: set up a SIGKILL handler for the pinger
+
+	$self->set_initial_status();
+
+	while (1) {
+		sleep($self->{ping_delay});
+
+		$self->do_ping();
+		$self->set_status();
+	}
+}
+
+
+sub set_initial_status {
+	my $self = shift;
+
+	$self->do_ping();
+	$self->{is_up} = $self->{failed_pings} > 0 ? 0 : 1;
+	$self->log_statuschange();
+}
+
+
+sub set_status {
+	my $self = shift;
+
+	$self->{old_status} = $self->{is_up};
+
+	if ($self->{pings} > $self->{allowed_failed_pings}) {
+		$self->{is_up} =
+			$self->{failed_pings} > $self->{allowed_failed_pings} ? 0 : 1;
+	}
+
+	$self->log_statuschange() if $self->{is_up} != $self->{old_status};
+}
+
+
+1;
---
+++ hostup.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+use Host;
+
+my %opt = (
+	config_path	=> "$ENV{HOME}/.hostsup.conf",
+	logfile		=> "$ENV{HOME}/.hostsup.log",
+);
+
+#GetOptions('c|config=s' => %opt{'config_path'})
+#	or die("Error in command line arguments\n");
+
+
+sub load_hosts {
+	my ($path) = @_;
+
+	my @hosts;
+
+	if (-f $path) {
+		open my $file, '<', $path or die $!;
+
+		while (my $line = <$file>) {
+			#if ($line =~ /$CONFIG_HOST/) {
+			if ($line =~ /\A \s* host \s+ (\S+) \s+ (\S+)/xms) {
+				push @hosts, Host->new($1, $2, $opt{logfile});
+			}
+		}
+
+		close $file;
+	}
+
+	return \@hosts;
+}
+
+
+sub start_pingers {
+	my $hosts_ref = shift;
+	my @pingers;
+
+	for my $host (@{$hosts_ref}) {
+		my $pid = fork;
+
+		if (not defined $pid) {
+			# TODO: kill all children
+			print STDERR "Fork failed, quitting\n";
+			exit 1;
+		} elsif ($pid == 0) {
+			# child process
+			$host->ping();
+		} else {
+			push @pingers, $pid;
+		}
+	}
+	return \@pingers;
+}
+
+
+my $hosts_ref = load_hosts($opt{config_path});
+my $pingers_ref = start_pingers($hosts_ref);
+
+
+# TODO: log program start. Check the logfile lock, too, and if file is locked
+# terminate. It implies that another instance is already running.
+# open my $logfh, '>>', $opt{logfile} or die $!;
+
+# wait for children to die
+# TODO: add signal handler to kill children
+while (-1 == wait) {}
