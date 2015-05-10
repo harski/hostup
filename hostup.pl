e5721ab266547127a2a2cd6ec74719fcc150aec2
Tuomo Hartikainen <tth@harski.org>
2015-05-10 20:54:50 +0300

    Add --help

--- hostup.pl
+++ hostup.pl
@@ -12,12 +12,33 @@ use Hostup::Util qw(log_str);
 
 my %opt = (
 	config_path	=> "$ENV{HOME}/.hostsup.conf",
+	help		=> 0,
 	logfile		=> "$ENV{HOME}/.hostsup.log",
 );
 
-GetOptions('c|config=s' => \$opt{'config_path'},
+GetOptions('c|config=s' => \$opt{config_path},
+	   'h|help|usage!'	=> \$opt{help},
 	   'l|log=s'	=> \$opt{logfile},
 	  ) or die("Error in command line arguments\n");
+
+
+sub action_hostup {
+	my $hosts_ref = load_hosts($opt{config_path});
+	my $pingers_ref = start_pingers($hosts_ref);
+
+	log_str($opt{logfile}, "hostup_service", "STARTED");
+
+	# wait for children to die
+	# TODO: add signal handler to kill children
+	while () {
+		my $ret = wait;
+		if ($ret == -1) {
+			last;
+		}
+
+		print "child '$ret' terminated\n";
+	}
+}
 
 
 sub load_hosts {
@@ -38,6 +59,18 @@ sub load_hosts {
 	}
 
 	return \@hosts;
+}
+
+
+sub print_help {
+	print <<HELP_END
+$0 [OPTION]...
+
+Options:
+  -c, --config CONFIG_FILE
+  -h, --help, --usage
+  -l, --log LOG_FILE
+HELP_END
 }
 
 
@@ -63,11 +96,8 @@ sub start_pingers {
 }
 
 
-my $hosts_ref = load_hosts($opt{config_path});
-my $pingers_ref = start_pingers($hosts_ref);
-
-log_str($opt{logfile}, "hostup_service", "STARTED");
-
-# wait for children to die
-# TODO: add signal handler to kill children
-while (-1 == wait) {}
+if ($opt{help}) {
+	print_help();
+} else {
+	action_hostup();
+}
